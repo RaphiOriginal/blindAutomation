40a7b18e5cfe8f084bcaebff5407e87aad7ac087
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-22 19:44:49 +0200

    extract setting classes

---
+++ settings/auth.py
@@ -0,0 +1,4 @@
+class Auth:
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
---
+++ settings/coordinates.py
@@ -0,0 +1,4 @@
+class Coordinates:
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
