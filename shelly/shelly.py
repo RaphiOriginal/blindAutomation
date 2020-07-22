8f187172d73807b136cbbcfd586949745b950df2
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-22 22:52:18 +0200

    adding actions

--- shelly/shelly.py
+++ shelly/shelly.py
@@ -6,5 +6,21 @@ class Shelly:
         self.id: str = id
         self.direction: str = direction
 
+    def getStatus(self):
+        self.__checkIP()
+        return '{}/status'.format(self.ip)
+
+    def getRoller(self):
+        self.__checkIP()
+        return '{}/roller/0'.format(self.ip)
+
+    def setRoller(self, pos):
+        self.__checkIP()
+        return '{}/roller/0?go=to_pos&roller_pos={}'.format(self.ip, pos)
+
+    def __checkIP(self):
+        if self.ip is None:
+            raise ValueError("IP must be set!")
+
     def __repr__(self):
         return 'Shelly: { name: %s, id: %s, direction: %s, ip: %s}' % (self.name, self.id, self.direction, self.ip)
