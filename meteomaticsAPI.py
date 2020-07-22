3625d29eb8ab3b45154f02cc6fcbdab634ba9277
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-22 18:17:24 +0200

    wrap meteomatics logic into class

--- meteomaticsAPI.py
+++ meteomaticsAPI.py
@@ -1,88 +1,94 @@
-import datetime
-import math
-
 import yaml
 import requests
 from dateutil import parser, tz
 
-settingsFile = 'settings.yaml'
-url = 'http://api.meteomatics.com/'
-fields = 'sunrise:sql,sunset:sql'
-field = 'sun_azimuth:d'
-json = 'json'
-csv = 'csv'
+from settings import Settings
 
-def readSettings():
-    with open(settingsFile, 'r') as stream:
-        return yaml.safe_load(stream)
+class MeteomaticsAPI:
 
-def getAuth():
-    settings = readSettings()
-    return settings.get('auth')
+    def __init__(self):
+        self.settingsFile = 'settings.yaml'
+        self.url = 'http://api.meteomatics.com/'
+        self.fields = 'sunrise:sql,sunset:sql'
+        self.field = 'sun_azimuth:d'
+        self.json = 'json'
+        self.csv = 'csv'
 
-def getCoordinates():
-    settings = readSettings()
-    return settings.get('coordinates')
+        self.settings = None
 
-def buildLocation():
-    coordinates = getCoordinates()
-    return '{},{}'.format(coordinates.get('lat'), coordinates.get('long'))
+    def getSettings(self):
+        if self.settings == None:
+            self.settings = Settings(self.settingsFile)
+        return self.settings
+
+    def getAuth(self):
+        self.settings = self.getSettings()
+        return self.settings.getAuth()
+
+    def getCoordinates(self):
+        self.settings = self.getSettings()
+        return self.settings.getCoordinates()
+
+    def buildLocation(self):
+        coordinates = self.getCoordinates()
+        return '{},{}'.format(coordinates.lat, coordinates.long)
 
 
-def getSunriseAndSunset():
-    try:
-        auth = getAuth()
-        user = auth.get('username')
-        password = auth.get('password')
+    def getSunriseAndSunset(self):
+        try:
+            auth = self.getAuth()
+            user = auth.user
+            password = auth.password
 
-        time = 'now'
-        location = buildLocation()
+            time = 'now'
+            location = self.buildLocation()
 
-        r = requests.get(url + time + '/' + fields + '/' + location + '/' + json, auth=(user, password))
+            r = requests.get(self.url + time + '/' + self.fields + '/' + location + '/' + self.json, auth=(user, password))
+
+            if r.status_code != 200:
+                print('Request failed, Status Code: ' + r.status_code)
+
+            values = r.json()
+            print(values)
+
+            sunrise = values.get('data')[0].get('coordinates')[0].get('dates')[0].get('value')
+            sunset = values.get('data')[1].get('coordinates')[0].get('dates')[0].get('value')
+
+            return (self.toDate(sunrise), self.toDate(sunset))
+        except yaml.YAMLError as exp:
+            print(exp)
+
+    def toDate(self, isoDate):
+        date = parser.parse(isoDate)
+        return date.astimezone(tz.tzlocal())
+
+    def getAzimuthTime(self, sunrise, sunset, azimuth):
+        auth = self.getAuth()
+        user = auth.user
+        password = auth.password
+
+        time = '{}--{}:PT1M'.format(sunrise.isoformat(), sunset.isoformat())
+        location = self.buildLocation()
+
+        r = requests.get(self.url + time + '/' + self.field + '/' + location + '/' + self.json, auth=(user, password))
 
         if r.status_code != 200:
-            print('Request failed, Status Code: ' + r.status_code)
+            print('Request failed, Status Code: ' + str(r.status_code))
 
-        values = r.json()
+        values = r.json().get('data')[0].get('coordinates')[0].get('dates')
+
         print(values)
 
-        sunrise = values.get('data')[0].get('coordinates')[0].get('dates')[0].get('value')
-        sunset = values.get('data')[1].get('coordinates')[0].get('dates')[0].get('value')
+        best = -1000.0
+        date = ''
+        for entry in values:
+            if abs(azimuth - best) > abs(azimuth - entry.get('value')):
+                best = entry.get('value')
+                date = entry.get('date')
 
-        return (toDate(sunrise), toDate(sunset))
-    except yaml.YAMLError as exp:
-        print(exp)
+        return self.toDate(date)
 
-def toDate(isoDate):
-    date = parser.parse(isoDate)
-    return date.astimezone(tz.tzlocal())
-
-def getAzimuthTime(sunrise, sunset, azimuth):
-    auth = getAuth()
-    user = auth.get('username')
-    password = auth.get('password')
-
-    time = '{}--{}:PT1M'.format(sunrise.isoformat(), sunset.isoformat())
-    location = buildLocation()
-
-    r = requests.get(url + time + '/' + field + '/' + location + '/' + json, auth=(user, password))
-
-    if r.status_code != 200:
-        print('Request failed, Status Code: ' + str(r.status_code))
-
-    values = r.json().get('data')[0].get('coordinates')[0].get('dates')
-
-    print(values)
-
-    best = -1000.0
-    date = ''
-    for entry in values:
-        if abs(azimuth - best) > abs(azimuth - entry.get('value')):
-            best = entry.get('value')
-            date = entry.get('date')
-
-    return toDate(date)
-
-(sunrise, sunset) = getSunriseAndSunset()
-print(getAzimuthTime(sunrise, sunset, 110.0))
-print(getAzimuthTime(sunrise, sunset, 290.0))
+api = MeteomaticsAPI()
+(sunrise, sunset) = api.getSunriseAndSunset()
+print(api.getAzimuthTime(sunrise, sunset, 110.0))
+print(api.getAzimuthTime(sunrise, sunset, 290.0))
