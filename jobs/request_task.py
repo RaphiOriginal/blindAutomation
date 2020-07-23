c06bb11d977b7c9106ed63a263b029ccf8cef445
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-23 21:41:50 +0200

    print task status

--- jobs/request_task.py
+++ jobs/request_task.py
@@ -10,4 +10,5 @@ class Requesttask:
         if r.status_code != 200:
             print('Call with {} failed with status {} and content {}'.format(self.__url, r.status_code, r.text))
             exit(1)
+        print('Task {} completed'.format(self.__url))
         exit(0)
