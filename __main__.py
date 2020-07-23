1ece6352672231cb6900a6c0dfc0a76669a22e72
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-23 22:45:48 +0200

    put all together for a test

--- __main__.py
+++ __main__.py
@@ -1,28 +1,23 @@
 import sched
 import time
-from datetime import datetime, timedelta
-from time import sleep
-
-import requests
 
 from jobs.job import Job
 from jobs.task import Task
 from meteomatics.meteomatics_api import MeteomaticsAPI
 from shelly import shelly_finder
 
-#api = MeteomaticsAPI()
-#(sunrise, sunset) = api.getSunriseAndSunset()
-#print(api.getAzimuthTime(sunrise, sunset, 110.0))
-#print(api.getAzimuthTime(sunrise, sunset, 290.0))
+api = MeteomaticsAPI()
+(sunrise, sunset) = api.get_sunrise_and_sunset()
+tilt = api.get_azimuth_time(sunrise, sunset, 110.0)
+up = api.get_azimuth_time(sunrise, sunset, 290.0)
 
 shellys = shelly_finder.collect()
-
-now = datetime.now()
-delta = timedelta(seconds=5)
-
-shelly = shellys[0]
 schedule = sched.scheduler(time.time)
-job = Job(now + delta, shelly, Task.TILT)
-job.schedule(schedule)
+
+for shelly in shellys:
+    Job(sunrise, shelly, Task.OPEN).schedule(schedule)
+    Job(sunset, shelly, Task.CLOSE).schedule(schedule)
+    Job(tilt, shelly, Task.TILT).schedule(schedule)
+    Job(up, shelly, Task.OPEN).schedule(schedule)
+
 schedule.run()
-print(now)
