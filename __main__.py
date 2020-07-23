99ac79667318d43b3e5a3430884789777275454e
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-23 21:42:58 +0200

    test scheduler

--- __main__.py
+++ __main__.py
@@ -1,18 +1,28 @@
+import sched
+import time
+from datetime import datetime, timedelta
 from time import sleep
 
 import requests
 
+from jobs.job import Job
+from jobs.task import Task
 from meteomatics.meteomatics_api import MeteomaticsAPI
 from shelly import shelly_finder
 
-api = MeteomaticsAPI()
-(sunrise, sunset) = api.getSunriseAndSunset()
-print(api.getAzimuthTime(sunrise, sunset, 110.0))
-print(api.getAzimuthTime(sunrise, sunset, 290.0))
+#api = MeteomaticsAPI()
+#(sunrise, sunset) = api.getSunriseAndSunset()
+#print(api.getAzimuthTime(sunrise, sunset, 110.0))
+#print(api.getAzimuthTime(sunrise, sunset, 290.0))
 
 shellys = shelly_finder.collect()
-print(shellys)
+
+now = datetime.now()
+delta = timedelta(seconds=5)
 
 shelly = shellys[0]
-r = requests.get(shelly.setRoller(2))
-print(r.text)
+schedule = sched.scheduler(time.time)
+job = Job(now + delta, shelly, Task.TILT)
+job.schedule(schedule)
+schedule.run()
+print(now)
