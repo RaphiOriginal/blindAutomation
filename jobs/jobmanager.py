03700490a969e57cfcfe1b185035b6e0b5ffdbae
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 09:23:51 +0200

    some refactoring

--- jobs/jobmanager.py
+++ jobs/jobmanager.py
@@ -8,16 +8,27 @@ from dateutil import tz
 from jobs.job import Job
 
 
-class Jobcollector:
+def now():
+    return datetime.now(tz.tzlocal())
+
+
+def delay(delta):
+    if isinstance(delta, timedelta):
+        time.sleep(delta.seconds)
+    if isinstance(delta, int):
+        time.sleep(delta)
+
+
+class JobManager:
     def __init__(self):
-        self.__schedule = sched.scheduler(self.__now, self.__delay)
+        self.__schedule = sched.scheduler(now, delay)
         self.__jobs = defaultdict(list)
 
     def add(self, job: Job):
         self.__jobs[job.get_id()].append(job)
         return self
 
-    def collect(self):
+    def prepare(self):
         """Removes all jobs in the past except for the last one, to ensure at least the last one will be executed!"""
         for shelly, jobs in self.__jobs.items():
             jobs.sort(key=lambda job: job.get_time())
@@ -37,12 +48,3 @@ class Jobcollector:
 
     def run(self):
         self.__schedule.run()
-
-    def __now(self):
-        return datetime.now(tz.tzlocal())
-
-    def __delay(self, delta):
-        if isinstance(delta, timedelta):
-            time.sleep(delta.seconds)
-        if isinstance(delta, int):
-            time.sleep(delta)
