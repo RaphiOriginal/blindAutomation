e5e72a431b684db50620b3982d68d0a24de3e647
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 19:56:06 +0200

    redesign tasks

--- jobs/task.py
+++ jobs/task.py
@@ -1,8 +1,61 @@
 #!/usr/bin/env python3
 from enum import Enum
 
+from blinds import blind_state
+from blinds.blind_state import State
+from shelly.shelly import Shelly
+
 
 class Task(Enum):
     CLOSE = 1
     OPEN = 2
     TILT = 3
+
+    def get_for(self, shelly: Shelly):
+        if self == self.OPEN:
+            return [(Open(shelly),)]
+        if self == self.TILT:
+            return [(PreTilt(shelly),), (Tilt(shelly),)]
+        return [(Close(shelly),)]
+
+
+class BaseTask:
+    def __init__(self, shelly: Shelly, target: State, position: int):
+        self.shelly: Shelly = shelly
+        self.__target: State = target
+        self.__position: int = position
+
+    def ready(self):
+        return True
+
+    def done(self):
+        state = blind_state.fetch_blindstate(self.shelly)
+        return state.state() == self.__target
+
+    def do(self):
+        return self.shelly.set_roller(self.__position)
+
+
+class Close(BaseTask):
+    def __init__(self, shelly: Shelly):
+        super().__init__(shelly, State.CLOSED, 0)
+
+
+class Open(BaseTask):
+    def __init__(self, shelly: Shelly):
+        super().__init__(shelly, State.OPEN, 100)
+
+
+class PreTilt(BaseTask):
+    def __init__(self, shelly: Shelly):
+        super().__init__(shelly, State.TILT, 0)
+
+
+class Tilt(BaseTask):
+    def __init__(self, shelly: Shelly):
+        super().__init__(shelly, State.TILT, 2)
+        self.__precondition: State = State.CLOSED
+
+    def ready(self):
+        state = blind_state.fetch_blindstate(self.shelly)
+        return state.state() == self.__precondition
