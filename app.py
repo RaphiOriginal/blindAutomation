99751f13537af8b68dbd06990689ecf1b8bf2611
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 17:17:25 +0200

    renaming and adding some __init__.py files

---
+++ app.py
@@ -0,0 +1,24 @@
+from jobs.job import Job
+from jobs.jobmanager import JobManager
+from jobs.task import Task
+from meteomatics.meteomatics_api import MeteomaticsAPI
+from shelly import shelly_finder
+
+
+def main():
+    api = MeteomaticsAPI()
+    sun = api.fetch_sundata()
+
+    shellys = shelly_finder.collect()
+    manager = JobManager()
+
+    for shelly in shellys:
+        manager.add(Job(sun.get_sunrise(), shelly, Task.OPEN)) \
+            .add(Job(sun.get_sunset(), shelly, Task.CLOSE)) \
+            .add(Job(sun.find_azimuth(110).time, shelly, Task.TILT)) \
+            .add(Job(sun.find_azimuth(290).time, shelly, Task.OPEN))
+
+    manager.prepare().run()
+
+
+main()
