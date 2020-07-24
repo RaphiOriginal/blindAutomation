27cd23d13e66a6222289c4505ea33f7d54bf7f81
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 07:35:22 +0200

    always run last past job and fixing __delay

--- jobs/jobcollector.py
+++ jobs/jobcollector.py
@@ -12,11 +12,9 @@ class Jobcollector:
     def __init__(self):
         self.__schedule = sched.scheduler(self.__now, self.__delay)
         self.__jobs = defaultdict(list)
-        self.__last_job = defaultdict(int)
 
     def add(self, job: Job):
         self.__jobs[job.get_id()].append(job)
-        self.__last_job[job.get_id()] += 1
         return self
 
     def collect(self):
@@ -25,14 +23,15 @@ class Jobcollector:
             jobs.sort(key=lambda job: job.get_time())
 
             future = list(filter(lambda job: job.get_time() > datetime.now(job.tzinfo), jobs))
-            if len(future) == 0:
-                last = jobs[self.__last_job[shelly] - 1]
+            past = list(filter(lambda job: job.get_time() < datetime.now(job.tzinfo), jobs))
+            if len(past) > 0:
+                last = past[len(past) - 1]
                 last.schedule(self.__schedule)
-                print('One job collected for {}: {}'.format(shelly, last))
-            else:
-                for job in future:
-                    job.schedule(self.__schedule)
-                print('{} jobs collected for {}: {}'.format(len(future), shelly, future))
+                print('One past job collected for {}: {}'.format(shelly, last))
+
+            for job in future:
+                job.schedule(self.__schedule)
+            print('{} jobs collected for {}: {}'.format(len(future), shelly, future))
 
         return self
 
@@ -42,5 +41,8 @@ class Jobcollector:
     def __now(self):
         return datetime.now(tz.tzlocal())
 
-    def __delay(self, delta: timedelta):
-        time.sleep(delta.seconds)
+    def __delay(self, delta):
+        if isinstance(delta, timedelta):
+            time.sleep(delta.seconds)
+        if isinstance(delta, int):
+            time.sleep(delta)
