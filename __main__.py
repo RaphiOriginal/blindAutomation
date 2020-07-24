2d8c3f6461f4129b31447f17d253181ebc33f6ad
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 09:23:41 +0200

    some refactoring

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import sched
 import time
 
 from jobs.job import Job
-from jobs.jobcollector import Jobcollector
+from jobs.jobmanager import JobManager
 from jobs.task import Task
 from meteomatics.meteomatics_api import MeteomaticsAPI
 from shelly import shelly_finder
@@ -11,12 +11,12 @@ api = MeteomaticsAPI()
 sun = api.fetch_sundata()
 
 shellys = shelly_finder.collect()
-collector = Jobcollector()
+manager = JobManager()
 
 for shelly in shellys:
-    collector.add(Job(sun.get_sunrise(), shelly, Task.OPEN))\
+    manager.add(Job(sun.get_sunrise(), shelly, Task.OPEN))\
         .add(Job(sun.get_sunset(), shelly, Task.CLOSE))\
         .add(Job(sun.find_azimuth(110).time, shelly, Task.TILT))\
         .add(Job(sun.find_azimuth(290).time, shelly, Task.OPEN))
 
-collector.collect().run()
+manager.prepare().run()
