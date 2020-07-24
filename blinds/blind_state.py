0149135e2008d8a442576e75e0609dfbee509e57
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-24 19:55:07 +0200

    fix state calculation and add repr

--- blinds/blind_state.py
+++ blinds/blind_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from enum import Enum
 
+import requests
+
 
 class Direction(Enum):
     OPEN = 'open'
@@ -30,10 +32,23 @@ class BlindState:
         self.__last_direction: Direction = Direction.from_name(direction)
 
     def state(self):
-        if self.__last_direction == Direction.CLOSE and self.__position == 0:
+        if self.__last_direction == Direction.CLOSE.value and self.__position == 0:
             return State.CLOSED
 
-        if self.__last_direction == Direction.OPEN and self.__position < 5:
+        if self.__last_direction == Direction.OPEN.value and self.__position < 5:
             return State.TILT
 
         return State.OPEN
+
+    def __repr__(self):
+        return 'BlindState: { position: %s, last_direction: %s, state: %s}' %\
+               (self.__position, self.__last_direction, self.state())
+
+
+def fetch_blindstate(shelly):
+    response = requests.get(shelly.get_roller())
+    if response.status_code == 200:
+        data = response.json()
+        return BlindState(data.get('current_pos'), data.get('last_direction'))
+    raise ConnectionError('Negative answer from shelly {}: {} - {}'
+                          .format(shelly.id, response.status_code, response.text))
