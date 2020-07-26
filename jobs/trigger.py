7821de1d04be80a34d100189172e0f7ca23a3706
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-26 20:04:08 +0200

    use wall for shelly triggers degree

--- jobs/trigger.py
+++ jobs/trigger.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 from datetime import datetime, time
 
 from jobs.job import Job
 from jobs.jobmanager import JobManager
 from jobs.task import Task
 from shelly.shelly import Shelly
+from shelly.wall import Wall
 from sun.sundata import Sundata
 
 
@@ -88,12 +90,12 @@ class TimeTrigger(Trigger):
 
 
 def apply_triggers(manager: JobManager, sundata: Sundata, shelly: Shelly):
-    triggers = extract_triggers(shelly.triggers, sundata)
+    triggers = extract_triggers(shelly.triggers, shelly.wall, sundata)
     for trigger in triggers:
         manager.add(Job(trigger, shelly))
 
 
-def extract_triggers(triggerdata, sundata: Sundata) -> [Trigger]:
+def extract_triggers(triggerdata, wall: Wall, sundata: Sundata) -> [Trigger]:
     triggers: [Trigger] = []
     for trigger in triggerdata:
         if trigger == SunriseTrigger.type():
@@ -105,16 +107,19 @@ def extract_triggers(triggerdata, sundata: Sundata) -> [Trigger]:
             triggers.append(sunset)
             continue
         if trigger == SunInTrigger.type():
-            sunin = SunInTrigger(sundata, 110)
+            sunin = SunInTrigger(sundata, wall.in_sun())
             triggers.append(sunin)
             continue
         if trigger == SunOutTrigger.type():
-            sunout = SunOutTrigger(sundata, 290)
+            sunout = SunOutTrigger(sundata, wall.out_sun())
             triggers.append(sunout)
             continue
-        if 'task' in trigger.keys() and 'time' in trigger.keys():
-            task = Task.from_name(trigger.get('task'))
-            runtime = time.fromisoformat(trigger.get('time'))
+        if TimeTrigger.type() in trigger.keys():
+            timetrigger = trigger.get(TimeTrigger.type())
+            task = Task.from_name(timetrigger.get('task'))
+            runtime = time.fromisoformat(timetrigger.get('time'))
+            t = TimeTrigger(runtime, task)
+            triggers.append(t)
             continue
         print('No Trigger for {} existing'.format(trigger))
     return triggers
