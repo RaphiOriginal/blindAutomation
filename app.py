1cdf0a5fd93dbf1e9a65351010532e36c4c3be9e
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-26 20:06:38 +0200

    update app to recent changes

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
-from jobs.job import Job
+from jobs import trigger
 from jobs.jobmanager import JobManager
-from jobs.task import Task
-from jobs.trigger import SunriseTrigger, SunsetTrigger, SunInTrigger, SunOutTrigger
 from meteomatics.meteomatics_api import MeteomaticsAPI
 from shelly import shelly_finder
 
 
 def main():
     api = MeteomaticsAPI()
+    shellys = shelly_finder.collect()
     sun = api.fetch_sundata()
 
-    shellys = shelly_finder.collect()
     manager = JobManager()
 
     for shelly in shellys:
-        manager.add(Job(SunriseTrigger(sun), shelly)) \
-            .add(Job(SunsetTrigger(sun), shelly)) \
-            .add(Job(SunInTrigger(sun, 110), shelly)) \
-            .add(Job(SunOutTrigger(sun, 290), shelly))
+        trigger.apply_triggers(manager, sun, shelly)
 
     manager.prepare().run()
 
