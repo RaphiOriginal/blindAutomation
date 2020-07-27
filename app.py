9f63844cc9fe7842bbce70e0ee4ea868f2081991
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-27 16:56:20 +0200

    adding basic config for logger

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+import logging
+
 from jobs import trigger
 from jobs.jobmanager import JobManager
 from meteomatics.meteomatics_api import MeteomaticsAPI
 from shelly import shelly_finder
 
 
+logger = logging.getLogger(__name__)
+
+
 def main():
+    logging.basicConfig(level=logging.DEBUG)
     api = MeteomaticsAPI()
     shellys = shelly_finder.collect()
     sun = api.fetch_sundata()
