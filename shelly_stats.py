1013eb080e7a926465db91050bb752bec9a810f4
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-07-28 17:55:41 +0200

    improve logging

--- shelly_stats.py
+++ shelly_stats.py
@@ -7,6 +7,7 @@ from shelly import shelly_finder
 def main():
     logging.basicConfig(level=logging.DEBUG)
     shellys = shelly_finder.collect()
+    logging.info(len(shellys))
     logging.info(shellys)
 
 
