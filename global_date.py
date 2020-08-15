8aa96004c3f12db3d7c91b25861206a14555e846
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-15 09:05:18 +0200

    reset date to get correct sunrise and sunset dates from library

--- global_date.py
+++ global_date.py
@@ -2,4 +2,10 @@ import datetime
 
 from dateutil.tz import tz
 
-date: datetime = datetime.datetime.now(tz.tzlocal())
+
+def build_date() -> datetime:
+    now = datetime.datetime.now(tz.tzlocal())
+    return now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+date: datetime = build_date()
