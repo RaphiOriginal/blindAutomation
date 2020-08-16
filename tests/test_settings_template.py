980c4a82f771fc4166d12bd64e47220c6b2f302d
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-16 15:41:43 +0200

    test schema test for travis

--- tests/test_settings_template.py
+++ tests/test_settings_template.py
@@ -5,8 +5,8 @@ from yamale import yamale
 
 class SettingsTest(unittest.TestCase):
     def test_settings_valid(self):
-        schema = yamale.make_schema('../schema.yaml')
-        data = yamale.make_data('../settings.yaml.template')
+        schema = yamale.make_schema('schema.yaml')
+        data = yamale.make_data('settings.yaml.template')
         yamale.validate(schema, data)
 
 
