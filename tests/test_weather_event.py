14d2f4b3b9df89811fcc011078b65a2e04c2b545
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-22 23:03:51 +0200

    adding test for that new Event generation

--- tests/test_weather_event.py
+++ tests/test_weather_event.py
@@ -1,8 +1,12 @@
 import json
 import unittest
 
+from building.blind import Blind
+from jobs.task import Open, Tilt
 from tests.mock.blind_mock import BlindMock
 from tests.mock.trigger_mock import TriggerMock
+from weather import event
+from weather.enum import WeatherConditionEnum, WeatherSubConditionEnum
 from weather.event import CloudsEvent, WeatherEvent
 from weather.weather import Weather
 
@@ -10,26 +14,64 @@ from weather.weather import Weather
 class WeatherEventCase(unittest.TestCase):
     def test_clouds(self):
         # Setup
-        event = CloudsEvent()
-        blind, trigger = self.__prepare([event], 804)
+        e = CloudsEvent()
+        b, trigger = self.__prepare([e], 804)
         # Test
-        blind.update(trigger)
+        b.update(trigger)
         # Check
-        self.assertEqual(1, blind.open_c)
+        self.assertEqual(1, b.open_c)
 
     @staticmethod
     def __prepare(events: [WeatherEvent], code: int) -> (BlindMock, TriggerMock):
-        blind = get_blind()
-        blind.add_events(events)
+        b = get_blind()
+        b.add_events(events)
         jsn = get_json()
         jsn['weather'][0]['id'] = code
         w = Weather(jsn)
         trigger = TriggerMock(w)
-        return blind, trigger
+        return b, trigger
+
+
+class WeatherEventBuilder(unittest.TestCase):
+    def test_clouds(self):
+        events = ['CLOUDY', {'CLOUDY': {'task': 'TILT', 'intensity': ['SCATTERED']}}]
+        b = blind(events)
+        event.apply_weather_events(b)
+        result = b._events
+        self.assertEqual(2, len(result))
+        self.assertEqual(Open.type(), result[0]._task.type())
+        self.assertEqual(Tilt.type(), result[1]._task.type())
+        item = result[0]
+        self.assertEqual(CloudsEvent.type(), item.type())
+        self.assertEqual(WeatherConditionEnum.CLOUDS, item._main)
+        self.assertEqual(1, len(item._sub))
+        self.assertEqual(WeatherSubConditionEnum.OVERCAST, item._sub[0])
+        item = result[1]
+        self.assertEqual(CloudsEvent.type(), item.type())
+        self.assertEqual(WeatherConditionEnum.CLOUDS, item._main)
+        self.assertEqual(1, len(item._sub))
+        self.assertEqual(WeatherSubConditionEnum.SCATTERED, item._sub[0])
+
+    def test_multi_sub(self):
+        events = [{'CLOUDY': {'intensity': ['SCATTERED', 'OVERCAST']}}]
+        b = blind(events)
+        event.apply_weather_events(b)
+        result = b._events[0]
+        self.assertEqual(1, len(b._events))
+        self.assertEqual(Open.type(), result._task.type())
+        self.assertEqual(CloudsEvent.type(), result.type())
+        self.assertEqual(WeatherConditionEnum.CLOUDS, result._main)
+        self.assertEqual(2, len(result._sub))
+        self.assertEqual(WeatherSubConditionEnum.SCATTERED, result._sub[0])
+        self.assertEqual(WeatherSubConditionEnum.OVERCAST, result._sub[1])
 
 
 def get_blind(name: str = 'Test') -> BlindMock:
     return BlindMock(name)
+
+
+def blind(events: []) -> Blind:
+    return Blind('test', 10, 20, None, [], events)
 
 
 def get_json():
