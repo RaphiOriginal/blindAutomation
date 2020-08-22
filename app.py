a2e08f5d781aa1bded90fb2e516446469cc24fcf
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-22 16:22:01 +0200

    clean up App code a bit 🧹

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 from logging import getLogger, DEBUG, INFO, WARNING, ERROR, WARN, CRITICAL, FATAL
 
 import global_date
+from api.api import ObservableSunAPI
 from building import building
 from jobs.jobmanager import manager
 from pvlibrary.pvlib_api import PVLibAPI
@@ -16,38 +17,36 @@ logger = getLogger(__name__)
 class App:
     def __init__(self, setting_file: str = 'settings.yaml', logging_level: str = 'INFO'):
         settings.load_settings(setting_file)
-        self.__api_pool: dict = self.__build_api_pool()
-        self.__api = self.__api_pool.get(settings.root.get('api').upper())
-        self.__weather_service = WeatherService()
-        self.__logging_levels: dict = self.__build_logging_levels()
-        self.__logging_level = self.__logging_levels.get(logging_level.upper())
+        self._api = self.__api(settings.root.get('api'))
+        self._weather_service = WeatherService()
+        self._level = self.__level(logging_level)
         self.__home = building.prepare()
-        logging.basicConfig(
-            format='%(asctime)s %(levelname)-8s %(message)s',
-            level=self.__logging_level,
-            datefmt='%Y-%m-%d %H:%M:%S')
 
     def run(self):
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            level=self._level,
+            datefmt='%Y-%m-%d %H:%M:%S')
         self.__attach_observers()
         while True:
-            self.__weather_service.start()
-            self.__api.fetch_sundata(global_date.date.next())
+            self._weather_service.start()
+            self._api.fetch_sundata(global_date.date.next())
             manager.prepare().run()
-            self.__weather_service.stop()
+            self._weather_service.stop()
 
     def __attach_observers(self):
         for blind in self.__home.blinds:
-            self.__api.attach(blind)
-            self.__weather_service.attach(blind)
+            self._api.attach(blind)
+            self._weather_service.attach(blind)
 
     @staticmethod
-    def __build_logging_levels() -> dict:
+    def __level(level: str) -> int:
         return {'DEBUG': DEBUG, 'INFO': INFO, 'WARNING': WARNING, 'WARN': WARN, 'ERROR': ERROR, 'CRITICAL': CRITICAL,
-                'FATAL': FATAL}
+                'FATAL': FATAL}[level.upper()]
 
     @staticmethod
-    def __build_api_pool() -> dict:
-        return {'PVLIB': PVLibAPI(), 'MOCK': SunAPIMock()}
+    def __api(api: str) -> ObservableSunAPI:
+        return {'PVLIB': PVLibAPI(), 'MOCK': SunAPIMock()}[api.upper()]
 
 
 def main():
