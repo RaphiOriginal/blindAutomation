f1354f55a0cdfa3bf00a56e37f582f13c5bbbb7c
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-23 20:30:51 +0200

    defining blocker object for events

--- event/event.py
+++ event/event.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, TypeVar, Generic
 
 
 class Event(ABC):
@@ -14,9 +16,55 @@ class Event(ABC):
         pass
 
     @abstractmethod
-    def do(self, on: Any) -> bool:
+    def do(self, on: Any) -> Blocker:
         """
         Execute the event
         :param on: Blinds to be moved
         :return: True if event was Successfully executed
         """
+
+
+T = TypeVar('T')
+
+
+class Blocker(ABC):
+
+    @abstractmethod
+    def block(self):
+        """
+        Will let the Blocker be blocking
+        """
+        pass
+
+    @abstractmethod
+    def unblock(self):
+        """
+        Will release Blocker from blocking
+        """
+        pass
+
+    @abstractmethod
+    def update(self, blocked: T):
+        """
+        Will tell the Blocker which task has been blocked, in case the task has to be applied after the blocking
+        :param blocked: Blocked Task
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def last(self) -> T:
+        """
+        Returns last blocked task
+        :return: last blocked Task
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def blocking(self) -> bool:
+        """
+        To check if Blocker is blocking
+        :return: true if Blocker should block
+        """
+        pass
