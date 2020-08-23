5c12107dc01c8ac724436a7fd27640a94ecbdfaf
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-23 21:03:37 +0200

    update blind mock

--- tests/mock/blind_mock.py
+++ tests/mock/blind_mock.py
@@ -1,13 +1,14 @@
-from typing import List
+from typing import List, Optional
 
 from building.interface import Shutter
 from building.state import State
-from event.event import Event
+from event.event import Event, Blocker
 
 
 class BlindMock(Shutter):
     def __init__(self, name: str):
         self.name: str = name
+        self.blocker: Optional[Blocker] = None
         self.events: [Event] = []
         self.open_c: int = 0
         self.close_c: int = 0
@@ -15,17 +16,18 @@ class BlindMock(Shutter):
         self.tilt_c: int = 0
         self.stats_c: int = 0
 
-    def open(self) -> bool:
-        self.open_c = self.open_c + 1
-        return True
+    def open(self) -> Optional[Blocker]:
+        if self.blocker is None or not self.blocker.blocking:
+            self.open_c = self.open_c + 1
+        return self.blocker
 
-    def close(self) -> bool:
+    def close(self) -> Optional[Blocker]:
         pass
 
-    def move(self, pos: int) -> bool:
+    def move(self, pos: int) -> Optional[Blocker]:
         pass
 
-    def tilt(self, degree: int) -> bool:
+    def tilt(self, degree: int) -> Optional[Blocker]:
         pass
 
     def stats(self) -> State:
@@ -62,4 +64,4 @@ class BlindMock(Shutter):
     def update(self, subject):
         for event in self.events:
             if event.applies(subject.trigger):
-                event.do(self)
+                self.blocker = event.do(self)
