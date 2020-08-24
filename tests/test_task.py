9a575f41756751d60b6d6afa56205dd129b5a123
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-24 16:42:16 +0200

    adding test to check done() for tilt

--- tests/test_task.py
+++ tests/test_task.py
@@ -2,7 +2,7 @@ import unittest
 
 from building.blind import Blind
 from building.state import State
-from jobs.task import Open, PreTilt, Close
+from jobs.task import Open, PreTilt, Close, Tilt
 from tests.mock.device import DeviceMock
 
 
@@ -24,6 +24,51 @@ class Task(unittest.TestCase):
         self.assertEqual(State.CLOSED, device.state)
         prepare = PreTilt(blind)
         self.assertTrue(prepare.done())
+
+    def test_done_pretilt_tilt_state(self):
+        device = DeviceMock('TestDevice')
+        blind = Blind('Test', 0, 0, device, [])
+        Tilt(blind).do()
+        self.assertEqual(1, device.tilt_counter)
+        self.assertEqual(State.TILT, device.state)
+        prepare = PreTilt(blind)
+        self.assertTrue(prepare.done())
+
+    def test_done_tilt_open_state(self):
+        device = DeviceMock('TestDevice')
+        blind = Blind('Test', 0, 0, device, [])
+        Open(blind).do()
+        self.assertEqual(1, device.open_counter)
+        self.assertEqual(State.OPEN, device.state)
+        tilt = Tilt(blind)
+        self.assertFalse(tilt.done())
+
+    def test_done_tilt_close_state(self):
+        device = DeviceMock('TestDevice')
+        blind = Blind('Test', 0, 0, device, [])
+        Close(blind).do()
+        self.assertEqual(1, device.close_counter)
+        self.assertEqual(State.CLOSED, device.state)
+        tilt = Tilt(blind)
+        self.assertFalse(tilt.done())
+
+    def test_done_tilt_tilt_state(self):
+        device = DeviceMock('TestDevice')
+        blind = Blind('Test', 0, 0, device, [])
+        Tilt(blind).do()
+        self.assertEqual(1, device.tilt_counter)
+        self.assertEqual(State.TILT, device.state)
+        tilt = Tilt(blind)
+        self.assertTrue(tilt.done())
+
+    def test_done_tilt_different_tilt_state(self):
+        device = DeviceMock('TestDevice')
+        blind = Blind('Test', 0, 0, device, [])
+        Tilt(blind, 45).do()
+        self.assertEqual(1, device.tilt_counter)
+        self.assertEqual(State.TILT, device.state)
+        tilt = Tilt(blind)
+        self.assertFalse(tilt.done())
 
 
 if __name__ == '__main__':
