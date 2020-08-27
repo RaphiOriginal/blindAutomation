e7c702f18a4ec49e1636b67882771d0fb42eaa0e
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-27 19:16:36 +0200

    further adjustments

--- tests/test_settings_template.py
+++ tests/test_settings_template.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import os
 import unittest
 
 from yamale import YamaleTestCase
 
 
 class SettingsTest(YamaleTestCase):
-    schema = 'blind_automation/schema.yaml'
-    yaml = 'blind_automation/settings.yaml.template'
+    def __init__(self):
+        print(os.getcwd())
+        self.schema = 'blind_automation/schema.yaml'
+        self.yaml = 'data/settings.yaml.template'
 
     def test_settings_valid(self):
         self.assertTrue(self.validate())
