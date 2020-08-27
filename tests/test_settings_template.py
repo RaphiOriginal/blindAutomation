934f875158704482d83d4988cb9c726cef4eeefd
Raphael Brunner <raphael@raphaelbrunner.ch>
2020-08-27 19:20:51 +0200

    add missing schema.yaml

--- tests/test_settings_template.py
+++ tests/test_settings_template.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-import os
 import unittest
 
 from yamale import YamaleTestCase
 
 
 class SettingsTest(YamaleTestCase):
-    def __init__(self):
-        print(os.getcwd())
-        self.schema = 'blind_automation/schema.yaml'
-        self.yaml = 'data/settings.yaml.template'
+    schema = 'blind_automation/schema.yaml'
+    yaml = 'data/settings.yaml.template'
 
     def test_settings_valid(self):
         self.assertTrue(self.validate())
